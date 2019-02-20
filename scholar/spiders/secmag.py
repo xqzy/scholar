64bfb8600f3d71fd4ebde73fb4d98d32730c5c1e
Ubuntu <ubuntu@ip-172-31-45-107.us-west-2.compute.internal>
2019-02-20 22:38:28 +0000

    First commit

---
+++ scholar/spiders/secmag.py
@@ -0,0 +1,18 @@
+
+from scrapy.spiders import XMLFeedSpider
+
+class Spider(XMLFeedSpider):
+    name = "secmag"
+    allowed_domains = ["securitymagazine.com"]
+    start_urls = ('https://www.securitymagazine.com/rss/topic/2246-banking-finance-insurance',)    #Crawl BPMX
+    itertag = 'item'
+
+    def parse_node(self, response, node):
+        self.logger.info('Hi, this is a <%s> node!: %s', self.itertag, ''.join(node.extract()))
+
+        item = {}
+        item['title'] = node.xpath('title/text()',).extract_first()                #define XPath for title
+        item['link'] = node.xpath('link/text()').extract_first()
+        item['pubDate'] = node.xpath('link/pubDate/text()').extract_first()
+        item['description'] = node.xpath('description/text()').extract_first()                #define XPath for description
+        return item
