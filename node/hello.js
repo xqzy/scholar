183a0c7b14c843f6d3c4140cb08663ddb0f3a7d7
EC2 Default User <ec2-user@ip-172-31-38-244.us-west-2.compute.internal>
2019-03-01 22:56:53 +0000

    nodejs work. url:/articles

--- node/hello.js
+++ node/hello.js
@@ -1,6 +1,26 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost/test';
+var str = "";
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080); 
+app.route('/articles').get(function(req, res) {
+   console.log("starting nodejs code ");
+   MongoClient.connect(url, function(err, client) {
+       console.log("connection ok");
+       const db = client.db('test');
+       var collection = db.collection('scholar');
+       var cursor = collection.find({});
+       str = "";
+       cursor.forEach(function(item) {
+           if (item != null) {
+                   str = str + "    Article id  " + item._id + "</br>";
+           }
+       }, function(err) {
+           res.send(str);
+           client.close();
+          }
+       );
+   });
+});
+var server = app.listen(8080, function() { });
--- scripts/install-node-stuff.sh
+++ scripts/install-node-stuff.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install 4.4.5
+nvm install v10.15.1
 npm i -g pm2@2.4.3
+npm install express
+npm install mongodb
 pm2 start ~/Code/scholar/node/hello.js
 :
