8187723b9074fa0bdbd2ee7ed84e2c36d5b9166d
EC2 Default User <ec2-user@ip-172-31-40-121.us-west-2.compute.internal>
2019-03-01 21:17:15 +0000

    streamlined node script

--- scripts/install-node-stuff.sh
+++ scripts/install-node-stuff.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 4.4.5
 npm i -g pm2@2.4.3
 pm2 start ~/Code/scholar/node/hello.js
 :
