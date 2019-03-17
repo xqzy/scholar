7d767e1a3c7d7eecf23aad3d2ef38a7e9b316285
EC2 Default User <ec2-user@ip-172-31-10-147.us-west-2.compute.internal>
2019-03-17 13:40:04 +0000

    mocha now avail from cli

--- scripts/install-node-stuff.sh
+++ scripts/install-node-stuff.sh
@@ -6,5 +6,8 @@ npm i -g pm2@2.4.3
 npm install express
 npm install mongodb
 npm install pug
+npm install -g mocha
+npm install chai
+npm install chai-http
 pm2 start ~/Code/scholar/node/hello.js
 :
