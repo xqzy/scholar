2116098998fb24e0bd7bb0d613753a02c4c7a400
EC2 Default User <ec2-user@ip-172-31-4-51.us-west-2.compute.internal>
2019-03-21 21:07:23 +0000

    import test database in test environment

--- scripts/import-db.sh
+++ scripts/import-db.sh
@@ -1,2 +1,26 @@
-aws s3 cp s3://thirtyfourtest/scholar.json /tmp/scholar.json
-mongoimport --db test --collection scholar --type json --file /tmp/scholar.json
+echo "########## starting import_db.sh ####" `date`
+. ./determine_environment
+if [ "$ENV" == "TEST" ] 
+then
+  BUCKET="thirtyfourtest"
+  DB="test"
+fi
+
+
+if [ "$ENV" == "DEV" ] 
+then
+  BUCKET="thirtyfourdev"
+  DB="dev"
+fi
+
+if [ "$ENV" == "DEV" -o "$ENV" == "TEST" ] 
+then
+  aws s3 cp s3://$BUCKET/scholar.json /tmp/scholar.json
+  ./mongo_delete_all_articles.sh
+  mongoimport --db $DB  --collection scholar --type json --file /tmp/scholar.json
+
+  echo "database imported"
+else
+  echo "database not imported, environment value not set (in tag ec2 instance"
+fi
+echo "########## exiting  import_db.sh ####" `date`
--- scripts/mongo_delete_all_articles.sh
+++ scripts/mongo_delete_all_articles.sh
@@ -1,7 +1,8 @@
+. ./determine_environment
 HOMEDIR=/home/ec2-user/Code/scholar
-if mongo < $HOMEDIR/scripts/mongo_delete_all_articles
+if mongo < $HOMEDIR/scripts/mongo_delete_all_articles.$ENV
 then
-  echo "Articles Successfully deleted"
+  echo "Articles Successfully deleted from $ENV database"
 else
 	echo ERROR deleting articles in Mongodb
 	exit 1
