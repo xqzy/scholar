181ff310c6e61dbeafe67358fe967fbec1234d09
EC2 Default User <ec2-user@ip-172-31-11-161.us-west-2.compute.internal>
2019-03-24 20:37:58 +0000

    improved test script, asynchronuous

--- node/test/smoketest.test.js
+++ node/test/smoketest.test.js
@@ -13,94 +13,103 @@ describe("smoke test", function() {
 });
 
 describe('Front end page', function(){
-  it('landing page exists', function(){
+  it('landing page exists', function(done){
     chai.request('http://localhost:8080')
     .get('/')
     .end(function(err, res) {
+      should.exist(res);
       res.should.have.status(200);
       res.should.be.html;
+      done();
     });
   });
 });
 
 
 describe('Main Article page ', function(){
-  it('article page exists', function(){
+  it('article page exists', function(done){
     chai.request('http://localhost:8080')
     .get('/articlez')
     .end(function(err, res) {
       res.should.have.status(200);
       res.should.be.html;
+      done();
     });
   });
 });
 
 describe('Deprecated Article page ', function(){
-  it('article page exists', function(){
+  it('article page exists', function(done){
     chai.request('http://localhost:8080')
 	.get('/articles')
 	.end(function(err, res) {
 	  res.should.have.status(200);
 	  res.should.be.html;
+          done();
 	});
   });
 });
 
 describe('Maintenance page ', function(){
-	  it('maintenance page exists', function(){
+	  it('maintenance page exists', function(done){
 	    chai.request('http://localhost:8080')
 	    .get('/maint')
 	    .end(function(err, res) {
 	      res.should.have.status(200);
 	      res.should.be.html;
+              done();
 	    });
 	  });
 	});
 
 
 describe('Database Admin page ', function(){
-	  it('dbadmin page exists', function(){
+	  it('dbadmin page exists', function(done){
 	    chai.request('http://localhost:8080')
 	    .get('/dbadmin')
 	    .end(function(err, res) {
 	      res.should.have.status(200);
 	      res.should.be.html;
+              done();
 	    });
 	  });
 	});
 
 
 describe('Delete Articles page ', function(){
-	  it('Deleta Articles page exists', function(){
+	  it('Deleta Articles page exists', function(done){
 	    chai.request('http://localhost:8080')
 	    .get('/deletearticles')
 	    .end(function(err, res) {
 	      res.should.have.status(200);
 	      res.should.be.html;
+              done();
 	    });
 	  });
 	});
 
 
 describe('Get Articles page ', function(){
-	  it('Get Articles page exists', function(){
+	  it('Get Articles page exists', function(done){
 	    chai.request('http://localhost:8080')
 	    .get('/getarticles')
 	    .end(function(err, res) {
 	      res.should.have.status(200);
 	      res.should.be.html;
+              done();
 	    });
 	  });
 	});
 
 
 describe('Recommend page ', function(){
-	  it('Recommend page exists', function(){
+	  it('Recommend page exists', function(done){
 	    chai.request('http://localhost:8080')
 	    .get('/recommend')
 	    .end(function(err, res) {
 	      res.should.have.status(200);
 	      res.should.be.html;
+              done();
 	    });
 	  });
 	});
