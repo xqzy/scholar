7995213c367a624c04c8729dabd2d8901ad42bb2
Rob <rob.schuurman@hccnet.nl>
2020-09-09 18:04:28 +0200

    debugged install-node_stuff script caused system not to start

--- scripts/install-node-stuff.sh
+++ scripts/install-node-stuff.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
+. ~/.nvm/nvm.sh
 chmod 755 ~/Code/scholar/scripts/*
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install v10.15.1
 npm i -g pm2@2.4.3
-npm install express
+npm install express-session
+npm install session-file-store
 npm install mongodb
 npm install pug
 npm install -g mocha
