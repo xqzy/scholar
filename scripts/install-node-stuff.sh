73919273e01863284e9a26fc13391e33f81a11ab
Rob <rob.schuurman@hccnet.nl>
2020-09-17 22:56:43 +0200

    merged bugfixing #30

--- scripts/install-node-stuff.sh
+++ scripts/install-node-stuff.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 . ~/.nvm/nvm.sh
+. ~/.bashrc
 chmod 755 ~/Code/scholar/scripts/*
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 . ~/.nvm/nvm.sh
