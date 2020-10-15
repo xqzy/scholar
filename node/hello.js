a6d5ba36605b87d5b9d709b8995c151a03252177
Rob <rob.schuurman@hccnet.nl>
2020-10-15 23:05:52 +0200

    npm package installation via npm install and package.json

--- node/hello.js
+++ node/hello.js
@@ -61,32 +61,6 @@ app.route('/sources').get(function(req,res){
     sourcesPage.getSourcesPage(req, res);
 });
 
-/*app.route('/articles').get(function(req, res) {
-   console.log("starting nodejs code ");
-   MongoClient.connect(url, function(err, client) {
-       console.log("connection ok");
-       const db = client.db(`${dbname}`);
-       var collection = db.collection('scholar');
-       var cursor = collection.find({});
-       str = "";
-       str = str + "<table> <tr> <th> link </th>";
-       str = str + "<tr> <th> scores  </th>";
-       str = str + "<th> description </th></tr>";
-                    
-       cursor.forEach(function(item) {
-           if (item != null) {
-                   if (item.score == null) { item.score = 0 }
-                   str = str + "<tr><td><a href=\"" + item.link + "\"> link </a>  </td><td>" + item.score +"</td><td>" +  item.description + "</br></td></tr>";
-           }
-       }, function(err) {
-           str = str + "</table>";
-           res.send(str);
-           client.close();
-          }
-       );
-   });
-});
-*/
 
 app.set('view engine', 'pug');
 // serve static files from the 'public' folder
--- scripts/install-node-stuff.sh
+++ scripts/install-node-stuff.sh
@@ -7,16 +7,23 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | b
 . ~/.nvm/nvm.sh
 nvm install v10.15.1
 npm i -g pm2@2.4.3
-npm install express
-npm install express-session
-npm install session-file-store
-npm install mongodb
-npm install mongoose
-npm install pug
-npm install -g mocha
-npm install chai
-npm install chai-http
-npm install dotenv
+
+# following installs all packages defined in package.json in node folder.
+npm install
+
+#   npm install express
+#   npm install express-session
+#   npm install session-file-store
+#   npm install mongodb
+#   npm install mongoose
+#   npm install passport
+#   npm install pug
+#   npm install -g mocha
+#   npm install chai
+#   npm install chai-http
+#   npm install dotenv
+
+# start the node application
 pm2 start ~/Code/scholar/node/hello.js >> app.log 2>&1
 
 # until so far...
