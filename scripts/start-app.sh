71a64bd177d079d253c4d7b13873892dc571bedc
Rob <rob.schuurman@hccnet.nl>
2020-11-14 08:11:19 +0100

    Task 29: new update of start/stop-app.sh scripts.

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,2 +1,1 @@
-su - ec2-user -c "cd ~/Code/scholar/node && pm2 delete hello.js 2> /dev/null &&  pm2 start hello.js"
-
+cd ~/Code/scholar/node && pm2 start hello.js
---
+++ scripts/stop-app.sh
@@ -0,0 +1,1 @@
+cd ~/Code/scholar/node && pm2 delete hello.js 2> /dev/null
