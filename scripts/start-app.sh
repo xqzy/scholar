4f4ff4f028bbad1344cbcdbf59f6b87bedf22773
Rob <rob.schuurman@hccnet.nl>
2020-11-14 09:16:47 +0100

    Task 29 : fix pipeline

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+# 
 su - ec2-user -c "cd ~/Code/scholar/node && pm2 delete hello.js 2> /dev/null &&  pm2 start hello.js"
--- scripts/stop-app.sh
+++ scripts/stop-app.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+# 
 su - ec2-user -c "cd ~/Code/scholar/node && pm2 delete hello.js 2> /dev/null"
 
 
