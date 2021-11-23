25b50df12a49186c92d74907edd65d31b534569f
Rob <rob.schuurman@hccnet.nl>
2021-11-23 22:24:42 +0100

    getarticles -> starting crawler within proper python envrionment pipelines -> issues wiht tabs/spaces now visible in pyth3

--- scripts/get_articles.sh
+++ scripts/get_articles.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-cd /home/ec2-user/Code/scholar/scholar
+cd /home/ec2-user
+source scrapy-env/bin/activate
+cd Code/scholar/scholar
 scrapy crawl bankinfosec
 scrapy crawl computerweekly
 scrapy crawl secmag
 scrapy crawl krebs
 scrapy crawl schneier
 scrapy crawl theregister
+deactivate
